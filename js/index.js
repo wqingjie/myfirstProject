874b48e9f4ae6bf4bd07d7f11fd0c3c596e2bb69
wqingjie <872779305@qq.com>
2016-06-18 10:33:00 +0800

    个人简历

--- js/index.js
+++ js/index.js
@@ -1,120 +1,75 @@
-var boxList = document.getElementById('boxList');
-var page = document.getElementById('page');
-var pageList = document.getElementById('pageList');
-var oLi = pageList.getElementsByTagName('li');
-var pageInput = document.getElementById('pageInput');
-var n = 1, totalPage = 0;
+//->动态计算REM的换算比例
+~function (desW) {
+    var winW = document.documentElement.clientWidth;
+    document.documentElement.style.fontSize = winW / desW * 100 + "px";
+}(640);
 
-bindHTML();
-function bindHTML() {
-    function callback(jsondata) {
-        var total = jsondata['total'], data = jsondata['data'];
-        totalPage = total;
-        var str = '';
-        for (var i = 0; i < data.length; i++) {
-            var curData = data[i];
-            var sex = curData['sex'] === 1 ? '女' : '男';
-            str += '<li>';
-            str += '<span>' + curData['num'] + '</span>';
-            str += '<span>' + curData['name'] + '</span>';
-            str += '<span>' + sex + '</span>';
-            str += '<span>' + curData['score'] + '</span>';
-            str += '</li>';
-        }
-        boxList.innerHTML = str;
-
-        if (pageList.innerHTML == '') {
-            str = '';
-            for (i = 1; i <= total; i++) {
-                if (i === 1) {
-                    str += '<li class="bg">' + i + '</li>';
-                    continue;
-                }
-                str += '<li>' + i + '</li>';
+//->给滑屏区域进行初始化设置
+~function () {
+    var step = 0, divList = null;
+    var swp = new Swiper(".swiper-container", {
+        loop: true,
+        direction: 'vertical',
+        onSlidePrevEnd: function () {
+            step--;
+            change();
+            if (step === 0) {
+                step = 5;
             }
-            pageList.innerHTML = str;
+        },
+        onSlideNextEnd: function () {
+            step++;
+            change();
+            if (step === 6) {
+                step = 1;
+            }
         }
+    });
 
-        for (var j = 0; j < oLi.length; j++) {
-            oLi[j].className = j == (n - 1) ? 'bg' : null;
-        }
-        pageInput.value = n;
+
+    function change() {
+        divList = document.querySelectorAll(".swiper-slide");
+        [].forEach.call(divList, function (curDiv, index) {
+            curDiv.id = index === step ? curDiv.getAttribute("trueId") : null;
+        });
     }
 
-    var xhr = new XMLHttpRequest;
-    xhr.open('get', '/getData?n=' + n + '&_=' + Math.random(), true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            callback(JSON.parse(xhr.responseText))
-        }
-    };
-    xhr.send(null);
-}
+}();
 
-page.onclick = function (e) {
-    e = e || window.event;
-    var tar = e.target || e.srcElement, tarTag = tar.tagName.toUpperCase();
-    if (tarTag === 'SPAN') {
-        if (tar.innerHTML == '首页') {
-            if (n === 1) {
-                return;
-            }
-            n = 1;
-        }
-        if (tar.innerHTML == '上一页') {
-            if (n === 1) {
-                return;
-            }
-            n--;
-        }
-        if (tar.innerHTML == '下一页') {
-            if (n === totalPage) {
-                return;
-            }
-            n++;
-        }
-        if (tar.innerHTML == '尾页') {
-            if (n === totalPage) {
-                return;
-            }
-            n = totalPage;
-        }
-        bindHTML();
-    }
-    if (tarTag === 'LI') {
-        var tempN = parseFloat(tar.innerHTML);
-        if (tempN == n) {
+//->音频的自动播放
+~function () {
+    var audioBox = document.querySelector(".audio"),
+        myAudio = audioBox.getElementsByTagName("audio")[0];
+
+    //->延时播放音频文件,先让其他的内容加载
+    window.setTimeout(function () {
+        myAudio.play();
+        myAudio.addEventListener("canplay", function () {
+            //->当音频可以播放的时候触发这个事件
+            audioBox.style.display = "block";
+            audioBox.className += " audioMove";
+        }, false);
+    }, 1000);
+
+    //->点击音乐图标,实现音频的暂停或者播放
+    audioBox.addEventListener("click", function () {
+        if (myAudio.paused) {//->当前是暂停的,我让其播放
+            myAudio.play();
+            audioBox.className = "audio audioMove";
             return;
         }
-        n = tempN;
-        bindHTML();
-    }
-};
+        //->当前是播放的,我让其暂停
+        myAudio.pause();
+        audioBox.className = "audio";
+    }, false);
+}();
 
-pageInput.onkeyup = function (e) {
-    e = e || window.event;
-    if (e.keyCode == '13') {
-        var num = Number(this.value.replace(/^ +| +$/g, ''));
-        if (isNaN(num)) {
-            this.value = n;
-            return;
-        }
-        if (num == n) {
-            this.value = n;
-            return;
-        }
-        if (num < 1) {
-            this.value = 1;
-            n = 1;
-        } else if (num > totalPage) {
-            this.value = totalPage;
-            n = totalPage;
-        } else {
-            n = num;
-        }
-        bindHTML();
-    }
-};
+
+
+
+
+
+
 
 
 
